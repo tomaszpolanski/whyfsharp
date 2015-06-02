5755195570c3717a3d6ac4caebb9357a6c4cd22c
Tomasz Polanski <tomasz.polanski@futurice.com>
2015-06-02 21:53:20 +0200

    Implementing examples

---
+++ WhyFSharp/WhyFSharp.CSharp/Program.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WhyFSharp.CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WhyFSharp.FSharp.Api.List.FoldTextToOneLineV4
+        }
+
+        public static string FoldTextToOneLineV1(string s, int maxLines = 8, string foldSeperator = ", ")
+        {
+            List<string> lines = new List<string>();
+            string[] strings = s.Split('\n', ',', ';');
+            foreach (string line in strings)
+            {
+                string trimmedLine = line.Trim();
+                if (trimmedLine.Length > 0)
+                {
+                    lines.Add(trimmedLine);
+                    if (lines.Count >= maxLines)
+                    {
+                        break;
+                    }
+                }
+            }
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < lines.Count; i++)
+            {
+                sb.Append(lines[i]);
+                if (i < lines.Count - 1)
+                {
+                    sb.Append(foldSeperator);
+                }
+            }
+
+            return sb.ToString();
+        }
+
+        public static string FoldTextToOneLineV2(string multiLineString, int maxLines = 8, string foldSeperator = ", ")
+        {
+            return multiLineString.Split('\n', ',', ';')
+                                  .Select(line => line.Trim())
+                                  .Where(trimmedLine => trimmedLine.Length > 0)
+                                  .Take(maxLines)
+                                  .Aggregate((first, second) => first + foldSeperator + second);
+        }
+    }
+}
---
+++ WhyFSharp/WhyFSharp.FSharp.Api/PortableLibrary1.fs
@@ -0,0 +1,31 @@
+﻿namespace WhyFSharp.FSharp.Api
+
+type List() = 
+    static let split l (s : string) = s.Split(l)
+    static let trim (s : string) = s.Trim()
+    
+    static let lift3 f x y z = 
+        match x with
+        | None -> None
+        | Some x' -> 
+            match y with
+            | None -> None
+            | Some y' -> 
+                match z with
+                | None -> None
+                | Some z' -> Some <| f x' y' z'
+    
+    static let foldTextToOneLine (multiLineString : string) maxLines foldSeperator = 
+        multiLineString
+        |> split [| '\n'; ','; ';' |]
+        |> Array.map trim
+        |> Array.filter ((<>) "")
+        |> Array.truncate maxLines
+        |> Array.fold (fun f s -> f + foldSeperator + s) ""
+    
+    static member FoldTextToOneLineV3 arg = arg |||> foldTextToOneLine
+
+    static member FoldTextToOneLineV4(?multiLineString, ?maxLines, ?foldSeperator) = 
+        match lift3 foldTextToOneLine multiLineString maxLines foldSeperator with
+        | Some s -> s
+        | None -> ""
